ce4f6120d90b9e7d4a439cf3d43b68ab897bcc5e
GuhanSundar <guhan.hybrid@gmail.com>
2016-09-23 23:14:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/alpha',function(req,res) {
-    res.send('Alpha Requested to be served here')
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/beta',function(req,res) {
