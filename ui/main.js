a6c766bee8668bf7326b035c68bc334724c28d03
GuhanSundar <guhan.hybrid@gmail.com>
2016-09-25 20:19:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,16 @@ function moveRight () {
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+ 'px'
 }
+
+function moveLeft () {
+    marginRight=marginRight+1;
+    img.style.marginRight=marginRight + 'px'
+} 
+
 img.onclick = function() {
+   var i=1;
+   if(i%2 === 0)
      var interval = setInterval(moveRight,20);
-    
+    else 
+      var interval1 = setInterval(moveLeft,20);
 }
