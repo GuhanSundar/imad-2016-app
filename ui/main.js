d4c31b3ea4ec484d4a90f8ee12a2fd5a1debc07e
GuhanSundar <guhan.hybrid@gmail.com>
2016-09-25 20:02:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,13 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 
+//CHanges the text of main Sectipn
 element.innerHTML = "Hasura";
+
+//Move the image
+var img = document.getElementById('madi');
+
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+    
+}
